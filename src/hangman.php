06aaf8493843ba1a7adfeb09cfc2e6ffb4b6ff27
Darío Rivera <fermius.us@gmail.com>
2020-06-08 14:19:01 -0500

    feat: add feedback for gueesed letters

--- src/hangman.php
+++ src/hangman.php
@@ -13,10 +13,39 @@ echo 'The secret word I\'m thinking of has ' . mb_strlen($secret_word) . ' lette
 const MAX_ATTEMPTS = 7;
 
 $attempts = 0;
+$letters_guessed = [];
 
 do {
     echo "Please guess a letter: " . PHP_EOL;
-    $letter = trim(fgets(STDIN));
+    $guess = trim(fgets(STDIN));
+
+    $letters = str_split_unicode($secret_word);
+
+    if (in_array($guess, $letters)) {
+        $letters_guessed[] = $guess;
+        echo 'Good guess: ' . get_guessed_word($secret_word, $letters_guessed) . PHP_EOL;
+    } else {
+        echo 'Oops!, that letter is not in the word: ' . get_guessed_word($secret_word, $letters_guessed) . PHP_EOL;
+    }
 
     $attempts++;
 } while ($attempts < MAX_ATTEMPTS);
+
+function str_split_unicode($str) {
+    return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+}
+
+function get_guessed_word($secret_word, $letters_guessed) {
+    $letters = str_split_unicode($secret_word);
+    $guessed_word = '';
+
+    foreach ($letters as $letter) {
+        if (!in_array($letter, $letters_guessed)) {
+            $guessed_word .= "_";
+        } else {
+            $guessed_word .= $letter;
+        }
+    }
+
+    return $guessed_word;
+}
