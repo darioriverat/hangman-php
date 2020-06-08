1cc98cf381a762db2497056ead9bbf408729a9ea
Darío Rivera <fermius.us@gmail.com>
2020-06-08 12:54:15 -0500

    feat: create round loop with user input interaction

--- src/hangman.php
+++ src/hangman.php
@@ -9,3 +9,14 @@ $secret_word = $argv[1];
 
 echo 'Welcome to the hangman game' . PHP_EOL;
 echo 'The secret word I\'m thinking of has ' . mb_strlen($secret_word) . ' letters' . PHP_EOL;
+
+const MAX_ATTEMPTS = 7;
+
+$attempts = 0;
+
+do {
+    echo "Please guess a letter: " . PHP_EOL;
+    $letter = trim(fgets(STDIN));
+
+    $attempts++;
+} while ($attempts < MAX_ATTEMPTS);
